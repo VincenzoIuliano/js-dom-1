7369274e9d1579c8756d51708e7de7d13fd67631
VincenzoIuliano <bubucane@outlook.it>
2024-10-22 16:54:00 +0200

    ADD: other solution

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 const button = document.querySelector('button');
 
+// SOLUZIONE CON ETICHETTA DEL BUTTON COME CONDIZIONE IF
+
+// button.addEventListener("click", function () {
+//     if (button.textContent==='Accendi') {
+//         lamp.src = "./img/yellow_lamp.png";
+//         button.textContent = 'Spegni'
+//         console.log('acceso')
+//     } else if (button.textContent==='Spegni') {
+//         lamp.src="./img/white_lamp.png"
+//         button.textContent='Accendi'
+//         console.log('spento')
+//     }
+// });
+
+
+const image = document.querySelector('img');
+
+// SOLUZIONE CON NOME CLASSE DELL'IMMAGINE COME CONDIZONE IF 
+
 button.addEventListener("click", function () {
-    if (button.textContent==='Accendi') {
+    if (image.className ==='lampOff') {
         lamp.src = "./img/yellow_lamp.png";
-        button.textContent = 'Spegni'
-        console.log('acceso')
-    } else if (button.textContent==='Spegni') {
-        lamp.src="./img/white_lamp.png"
-        button.textContent='Accendi'
-        console.log('spento')
+        button.textContent = 'Spegni';
+        image.classList.replace("lampOff", "lampOn");
+        console.log('acceso');
+    } else if (image.className ==='lampOn') {
+        lamp.src="./img/white_lamp.png";
+        button.textContent='Accendi';
+        image.classList.replace("lampOn", "lampOff");
+        console.log('spento');
     }
 });
-// button.addEventListener("click", turnLampOff);
-
-// function turnLampOn() {
-//     lamp.src="./img/yellow_lamp.png";
-//     button.textContent='Spegni'
-//     console.log('acceso')
-// }
-
-// button.addEventListener("click", turnLampOff);
-
-// function turnLampOff() {
-//     lamp.src="./img/white_lamp.png"
-//     button.textContent='Accendi'
-//     console.log('spento')
-// }
